7f028ea72336799ca3bf2ff97e0736e1812d5cea
Sergey Zabelin <zfix.amursk@ya.ru>
2022-10-26 00:41:34 +1000

    Home work 1

---
+++ app/src/main/java/ru/zfix27r/gettingandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.zfix27r.gettingandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
